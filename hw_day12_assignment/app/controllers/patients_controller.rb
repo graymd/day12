e6761df8edfda59dfffd96ac0e9c85bd6a10238b
Malachi Gray <graymd.cfi@gmail.com>
2015-01-21 15:52:47 -0500

    all CRUD completed for HW.  Need to create validation methods and update readme file.

--- hw_day12_assignment/app/controllers/patients_controller.rb
+++ hw_day12_assignment/app/controllers/patients_controller.rb
@@ -1,4 +1,9 @@
 class PatientsController < ApplicationController
+  def show
+    @patient = Patient.find params[:id]
+  end
+
+
   def index
     @patients = Patient.all
   end
@@ -11,6 +16,23 @@ class PatientsController < ApplicationController
     @patient = Patient.create patient_params
     redirect_to patients_path
   end
+
+  def edit
+    @patient = Patient.find params[:id]
+  end
+
+  def update
+    @patient = Patient.find params[:id]
+    @patient.update_attributes patient_params
+    redirect_to patients_path
+  end
+
+  def destroy
+  @patient = Patient.find params[:id]
+  @patient.delete
+  redirect_to patients_path
+  end
+
 
 private
   def patient_params
